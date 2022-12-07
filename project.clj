6ef19a6689367aef4027d397e10c2f856ec94f11
Artem Belankov <temanmd@gmail.com>
2022-12-07 10:39:45 +0600

    Add all needed deps

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-jetty-adapter "1.9.6"]
+                 [metosin/reitit-core "0.5.18"]
+                 [metosin/reitit-ring "0.5.18"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot todomvc.core
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.9.0"]]
