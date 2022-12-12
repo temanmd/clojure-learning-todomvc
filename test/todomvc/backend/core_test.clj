96224b73ee960152cd143748e83ad0ea74a68798
Artem Belankov <temanmd@gmail.com>
2022-12-12 12:48:10 +0600

    Setup tests dir structure

--- test/todomvc/backend/core_test.clj
+++ test/todomvc/backend/core_test.clj
@@ -1,6 +1,6 @@
-(ns todomvc.core-test
+(ns todomvc.backend.core-test
   (:require [clojure.test :refer :all]
-            [todomvc.core :refer :all]))
+            [todomvc.backend.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
